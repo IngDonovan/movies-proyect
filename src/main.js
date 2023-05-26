638783cc7683b7ccdfc9a396426b691274a4a6f3
Donovan Rojas <rojasdrm03@gmail.com>
2023-05-26 16:27:19 -0500

    add trending list with JS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,43 @@
-const apiKey = API_KEY;
+const api_key = API_KEY;
+const urlTrending = 'https://api.themoviedb.org/3/trending/movie/day';
 
-console.log(apiKey);
+async function getTrendingMoviesPreview() {
+    try {
+        const res = await fetch(urlTrending + api_key);
+        const data = await res.json();
+
+        const movies = data.results;
+        console.log({data, movies});
+
+        movies.forEach(movie => {
+            const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+            trendingPreviewMoviesContainer.innerHTML = '';
+            
+            // const movieContainer = document.createElement('div');
+            // movieContainer.classList.add('movie-container');
+            // const movieImg = document.createElement('img');
+            // movieImg.classList.add('movie-img');
+            // movieImg.setAttribute('alt', movie.title);
+            // movieImg.setAttribute(
+            //     'src',
+            //     'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+            // );
+
+            // movieContainer.appendChild(movieImg);
+            // trendingPreviewMoviesContainer.appendChild(movieContainer);
+
+            movies.forEach(movie => {
+                trendingPreviewMoviesContainer.insertAdjacentHTML('beforeend', `
+                  <div class='movie-container'>
+                    <img src='https://image.tmdb.org/t/p/w300/${movie.poster_path}' class='movie-img' alt='${movie.title}' />
+                  </div>
+                `)
+              });
+        });
+
+    } catch (error) {
+        console.log('Ocurrió un error: ', error);
+    }
+}
+
+getTrendingMoviesPreview();
