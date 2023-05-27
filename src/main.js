a6a4f1c0523f6021de3bfa3d01f68032b37bb572
Donovan Rojas <rojasdrm03@gmail.com>
2023-05-27 12:29:51 -0500

    add script axios to replace fetch

--- src/main.js
+++ src/main.js
@@ -1,62 +1,81 @@
 const api_key = API_KEY;
 const url = 'https://api.themoviedb.org/3/';
 const trending = 'trending/movie/day';
-const idiom = 'es'
+const idiom = 'es-ES'
 const urlenguage = `&language=${idiom}`;
 const urlCategories = 'genre/movie/list';
 
+const api = axios.create({
+  baseURL: url,
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  },
+  params: {
+    'api_key': KEY_AXIOS,
+    'language': idiom,
+  },
+});
 async function getTrendingMoviesPreview() {
-    try {
-        const res = await fetch(url + trending + api_key + urlenguage);
-        const data = await res.json();
+  try {
+      const {data} = await api(trending);
 
-        const movies = data.results;
-        //console.log({data, movies});
-        
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
-        trendingPreviewMoviesContainer.innerHTML = '';
-        
-        //movies.forEach(movie => {
-            
-            // const movieContainer = document.createElement('div');
-            // movieContainer.classList.add('movie-container');
-            // const movieImg = document.createElement('img');
-            // movieImg.classList.add('movie-img');
-            // movieImg.setAttribute('alt', movie.title);
-            // movieImg.setAttribute(
-            //     'src',
-            //     'https://image.tmdb.org/t/p/w300' + movie.poster_path,
-            // );
+      const movies = data.results;
+      //console.log({data, movies});
+      
+      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+      trendingPreviewMoviesContainer.innerHTML = '';
+      
+      movies.forEach(movie => {
+          trendingPreviewMoviesContainer.insertAdjacentHTML('beforeend', `
+            <div class='movie-container'>
+              <img src='https://image.tmdb.org/t/p/w300/${movie.poster_path}' class='movie-img' alt='${movie.title}' />
+            </div>
+          `)
+      });
+      
 
-            // movieContainer.appendChild(movieImg);
-            // trendingPreviewMoviesContainer.appendChild(movieContainer);
-          //});
-        movies.forEach(movie => {
-            trendingPreviewMoviesContainer.insertAdjacentHTML('beforeend', `
-              <div class='movie-container'>
-                <img src='https://image.tmdb.org/t/p/w300/${movie.poster_path}' class='movie-img' alt='${movie.title}' />
-              </div>
-            `)
-        });
-        
-
-    } catch (error) {
-        console.log('Ocurrió un error: ', error);
-    }
+  } catch (error) {
+      console.log('Ocurrió un error: ', error);
+  }
 }
+
+// async function getTrendingMoviesPreview() {
+//     try {
+//         const res = await fetch(url + trending + api_key + urlenguage);
+//         const data = await res.json();
+
+//         const movies = data.results;
+//         //console.log({data, movies});
+        
+//         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+//         trendingPreviewMoviesContainer.innerHTML = '';
+        
+//         movies.forEach(movie => {
+//             trendingPreviewMoviesContainer.insertAdjacentHTML('beforeend', `
+//               <div class='movie-container'>
+//                 <img src='https://image.tmdb.org/t/p/w300/${movie.poster_path}' class='movie-img' alt='${movie.title}' />
+//               </div>
+//             `)
+//         });
+        
+
+//     } catch (error) {
+//         console.log('Ocurrió un error: ', error);
+//     }
+// }
 
 async function getCategoriesPreview() {
   try {
-      const res = await fetch(url + urlCategories + api_key + urlenguage);
-      const data = await res.json();
+      const {data} = await api(urlCategories);
 
       const categories = data.genres;
-      console.log({data, categories});
-      
+      // console.log({data, categories});
+
       const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
       previewCategoriesContainer.innerHTML = '';
       
       categories.forEach(category => {
+        
         previewCategoriesContainer.insertAdjacentHTML('beforeend', `
             <div class="category-container">
               <h3 id="id${category.id}" class="category-title">${category.name}</h3>
@@ -68,6 +87,30 @@ async function getCategoriesPreview() {
       console.log('Ocurrió un error: ', error);
   }
 }
+
+// async function getCategoriesPreview() {
+//   try {
+//       const res = await fetch(url + urlCategories + api_key + urlenguage);
+//       const data = await res.json();
+
+//       const categories = data.genres;
+//       console.log({data, categories});
+      
+//       const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+//       previewCategoriesContainer.innerHTML = '';
+      
+//       categories.forEach(category => {
+//         previewCategoriesContainer.insertAdjacentHTML('beforeend', `
+//             <div class="category-container">
+//               <h3 id="id${category.id}" class="category-title">${category.name}</h3>
+//             </div>
+//           `)
+//       });
+      
+//   } catch (error) {
+//       console.log('Ocurrió un error: ', error);
+//   }
+// }
 
 
 getTrendingMoviesPreview();
